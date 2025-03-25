929af067100075b0d086c6d9fb923aed2d42e362
Ashit-Negi <sashitnegi@gmail.com>
2025-03-25 10:32:11 +0530

    home page

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,18 +1,54 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../redux/loaderSlice";
 import { getCurrentUser } from "../apicalls/user";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../redux/userSlice";
+import { Layout, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
+import {
+  UserOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  ProductOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
 
 function ProtectedRoute({ children }) {
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const navItems = [
+    {
+      label: "Home",
+      icon: <HomeOutlined />,
+    },
+    {
+      label: `${user ? user.name : " "}`,
+      icon: <UserOutlined />,
+      children: [
+        {
+          label: "Profile",
+          icon: <ProfileOutlined />,
+        },
+        {
+          label: (
+            <Link to="/login" onClick={() => localStorage.removeItem("token")}>
+              Log Out
+            </Link>
+          ),
+          icon: <LoginOutlined />,
+        },
+      ],
+    },
+  ];
 
   const getValidUser = async () => {
     try {
       dispatch(showLoading());
       const response = await getCurrentUser();
-      console.log(response);
+      dispatch(setUser(response.data)); // this is for to use the user data anywhere in we need user detail so we are saving this inside user state
       dispatch(hideLoading());
     } catch (error) {
       console.log(error);
@@ -26,7 +62,33 @@ function ProtectedRoute({ children }) {
       navigate("/login");
     }
   }, []);
-  return <div>{children}</div>;
+  return (
+    user && (
+      <>
+        <Layout>
+          <Header
+            className="d-flex justify-content-between"
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
+              Book My Show
+            </h3>
+            <Menu theme="dark" mode="horizontal" items={navItems}></Menu>
+          </Header>
+          <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
+            {children}
+          </div>
+        </Layout>
+      </>
+    )
+  );
 }
 
 export default ProtectedRoute;
