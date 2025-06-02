65dc724cb8a24f32d830c81fad3509201ee6ba23
Ashit-Negi <sashitnegi@gmail.com>
2025-06-02 13:54:57 +0530

    render url of backend

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// You can use environment variable to switch between local and production
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5003";
+
 export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
-}); // to communicate with backend(get,post,etc)
+});
